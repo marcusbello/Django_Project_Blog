c663ed8256bff1f1332828b93616e5fa68569220
“r4pm4ck” <normanziggy12@gmail.com>
2019-07-31 13:49:22 -0400

    Updated Settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,5 +148,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'kirkmanjack11@gmail.com'
 
 EMAIL_HOST_PASS = 'Unilorinpals@1'
+
+django_heroku.settings(locals())
 
 
